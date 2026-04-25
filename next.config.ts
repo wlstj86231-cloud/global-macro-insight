0b959417fb506d56bfe914fff3e0b664b3ea5e64
wlstj86231-cloud <wlstj86231@gmail.com>
2026-04-25 21:28:27 +0900

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,5 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  images: { remotePatterns: [{ protocol: "https", hostname: "lh3.googleusercontent.com" }] },
+};
+export default nextConfig;
