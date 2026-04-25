363a27f9b3b70d83628877671b10fbe377fcfa36
wlstj86231-cloud <wlstj86231@gmail.com>
2026-04-26 04:39:45 +0900

    Add components/ui/label.tsx

---
+++ components/ui/label.tsx
@@ -0,0 +1,7 @@
+"use client"
+import * as React from "react"
+import { cn } from "@/lib/utils"
+function Label({ className, ...props }: React.ComponentProps<"label">) {
+  return <label data-slot="label" className={cn("text-sm font-medium leading-none",className)} {...props}/>
+}
+export { Label }
