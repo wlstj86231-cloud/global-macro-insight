557735480aafb50c990c7a9034c9a7bf024de172
wlstj86231-cloud <wlstj86231@gmail.com>
2026-04-28 06:31:24 +0900

    Simplify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: { remotePatterns: [{ protocol: "https", hostname: "lh3.googleusercontent.com" }] },
-  experimental: {},
-  webpack: (config) => { return config; },
 };
 export default nextConfig;
