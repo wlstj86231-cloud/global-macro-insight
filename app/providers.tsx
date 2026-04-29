a2612ab4e1e1a6e5e32721280a4e795b995d2271
wlstj86231-cloud <wlstj86231@gmail.com>
2026-04-30 04:33:35 +0900

    Simplify app/providers.tsx: remove next-auth

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { SessionProvider } from "next-auth/react";
 import React from "react";
-export function Providers({children}:{children:React.ReactNode}) {
-  return <SessionProvider>{children}</SessionProvider>;
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
 }
